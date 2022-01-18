299b219aac92f0332c222f931579d36aa2fd6bb6
maurice kuehl <maurice.kuehl@protonmail.com>
2022-01-18 18:07:05 +0100

    added block school ip from /api and added protection for /api via requesting a session id on first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
+const Crypto = require("crypto");
 
 const port = 8000;
 app.listen(port, () => {
@@ -10,12 +11,36 @@ app.listen(port, () => {
 app.use(express.static("static"));
 app.use(express.json());
 
+let session = {};
 app.get("/api", (request, response) => {
-  fs.readFile("count.json", (err, data) => {
-    let d = JSON.parse(data);
-    d.count = d.count + 1;
-    fs.writeFile("count.json", JSON.stringify(d), "utf-8", (err, data) => {
-      response.json(d.count);
+  if (request.header("x-forwarded-for") === "79.226.116.160") {
+    //school ip
+    response.status(403).send();
+  } else if (request.header("Authorization") in session) {
+    delete session[request.header("Authorization")];
+    fs.readFile("count.json", (err, data) => {
+      let d = JSON.parse(data);
+      d.count = d.count + 1;
+      fs.writeFile("count.json", JSON.stringify(d), "utf-8", (err, data) => {
+        response.json(d.count);
+      });
     });
-  });
+  } else {
+    response.status(403).send();
+  }
 });
+
+app.get("/api/session", (request, response) => {
+  const sessionID = Crypto.randomBytes(20).toString("hex");
+  const date = new Date();
+  session[sessionID] = date.getTime();
+  response.json({ sessionID: sessionID });
+});
+
+const interval = setInterval(function () {
+  const date = new Date();
+  let time = date.getTime() - 100000;
+  Object.keys(session).map(function (key, index) {
+    if (session[key] < time) delete session[key];
+  });
+}, 10000);
